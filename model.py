90cd8bcb3fcfe642fd92964f5b56790efeb55351
Kaustubh Vyas <56315008+vyaskaustubh@users.noreply.github.com>
2021-05-22 04:00:38 +0530

    Update model.py

--- model.py
+++ model.py
@@ -89,7 +89,7 @@ class MLTModel(object):
         self.char_ids = tf.placeholder(tf.int32, [None, None, None], name="char_ids")
         self.sentence_lengths = tf.placeholder(tf.int32, [None], name="sentence_lengths")
         self.word_lengths = tf.placeholder(tf.int32, [None, None], name="word_lengths")
-        self.sentence_labels = tf.placeholder(tf.float32, [None,], name="sentence_labels")
+        self.sentence_labels = tf.placeholder(tf.float32, [None], name="sentence_labels")
         self.word_labels = tf.placeholder(tf.float32, [None,None], name="word_labels")
         self.word_objective_weights = tf.placeholder(tf.float32, [None,None], name="word_objective_weights")
         self.sentence_objective_weights = tf.placeholder(tf.float32, [None], name="sentence_objective_weights")
